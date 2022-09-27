2e8eef589d4e528dcdc68944f1320608802bc27f
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-09-27 17:06:41 +0200

    refacto

--- abo_data_formatting.py
+++ abo_data_formatting.py
@@ -8,7 +8,7 @@ class ABO:
         self.path_to_abo_dataset_folder = path_to_abo_dataset_folder
         self.gathered_data = {"main_image_id": [], "product_type": [], "color": [], "image_path": []}
 
-    def build_exploitable_dataset_from_raw_data(self):
+    def read_metadata(self):
         for json_file in os.listdir(self.path_to_abo_dataset_folder+"/listings/metadata"):
             f = os.path.join(self.path_to_abo_dataset_folder+"/listings/metadata",json_file)
             print(json_file)
@@ -25,6 +25,13 @@ class ABO:
                             self.gathered_data["color"].append(np.nan)
                     else:
                         self.gathered_data["color"].append(np.nan)
+
+    def map_metadata_to_images(self):
+        pass
+
+    def build_exploitable_dataset_from_raw_data(self):
+        self.read_metadata()
+        self.map_metadata_to_images()
         print(self.gathered_data["main_image_id"][0:5], self.gathered_data["product_type"][0:5], self.gathered_data["color"][0:5], self.gathered_data["image_path"][0:5])
 
 ABO("abo_dataset").build_exploitable_dataset_from_raw_data()

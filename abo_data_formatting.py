33dbcd25b77e0dac01c69843f75f89596cff82a5
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-09-28 10:31:23 +0200

    save gathered data to csv

--- abo_data_formatting.py
+++ abo_data_formatting.py
@@ -15,7 +15,6 @@ class ABO:
     def read_metadata(self):
         for json_file in os.listdir(self.path_to_abo_dataset_folder+"/listings/metadata"):
             f1 = os.path.join(self.path_to_abo_dataset_folder+"/listings/metadata",json_file)
-            print(json_file)
             with gzip.open(self.path_to_abo_dataset_folder+"/listings/metadata/"+json_file, 'r') as f:
                 data = [json.loads(line) for line in f]
             for product in data:
@@ -32,15 +31,24 @@ class ABO:
 
     def map_metadata_to_images(self):
         images_metadata_df = pd.read_csv(self.path_to_abo_dataset_folder+"/images/metadata/images.csv.gz", compression='gzip')
-        n=len(self.gathered_data["main_image_id"])
-        printProgressBar(0,n, length=50)
-        for i in range(n):
+        images_number=len(self.gathered_data["main_image_id"])
+        printProgressBar(0,images_number, length=50)
+        for i in range(images_number):
             self.gathered_data["image_path"].append(images_metadata_df[images_metadata_df["image_id"]==self.gathered_data["main_image_id"][i]]["path"].item())
-            printProgressBar(i+1, n, length=50)
+            printProgressBar(i+1, images_number, length=50)
+
+    def write_to_csv(self):
+        with open("gathered_abo_data.csv", "w") as f:
+            writer = csv.writer(f)
+            key_list = list(self.gathered_data.keys())
+            column_number = len(key_list)
+            writer.writerow(self.gathered_data.keys())
+            for feature in range(column_number):
+                writer.writerow([self.gathered_data[key][feature] for key in key_list])
 
     def build_exploitable_dataset_from_raw_data(self):
         self.read_metadata()
         self.map_metadata_to_images()
-        print(self.gathered_data["main_image_id"][0:5], self.gathered_data["product_type"][0:5], self.gathered_data["color"][0:5], self.gathered_data["image_path"][0:5])
+        self.write_to_csv()
 
 ABO("abo_dataset").build_exploitable_dataset_from_raw_data()

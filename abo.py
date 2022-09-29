01bf56d5074df03e9f451e1bb21d47731f759fb2
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-09-29 11:12:22 +0200

    take PR review into account

--- abo.py
+++ abo.py
@@ -15,12 +15,23 @@ class ABO(FewShotDataset):
     def __init__(
         self,
         root: Union[Path, str],
-        specs_file: Union[Path, str] = f"{ROOT_FOLDER}/gathered_abo_data.csv",
+        specs_file: Union[Path, str] = ROOT_FOLDER / "gathered_abo_data.csv",
         image_size: int = 84,
         transform: Callable = None,
         training: bool = False,
     ):
-        self.root = f"{ROOT_FOLDER}/{root}"
+        """
+        Args:
+            root: directory where all the images are
+            specs_file: path to the CSV file
+            image_size: images returned by the dataset will be square images of the given size
+            transform: torchvision transforms to be applied to images. If none is provided,
+                we use some standard transformations including ImageNet normalization.
+                These default transformations depend on the "training" argument.
+            training: preprocessing is slightly different for a training set, adding a random
+                cropping and a random horizontal flip. Only used if transforms = None.
+        """
+        self.root = ROOT_FOLDER / root
         self.data = self.load_specs(specs_file)
         self.class_names = list(self.data["product_type"].unique())
         self.transform = transform if transform else default_transform(image_size, training=training)
@@ -35,7 +46,7 @@ class ABO(FewShotDataset):
         return data.assign(label=lambda df: df["product_type"].map(label_mapping))
 
     def __getitem__(self, item: int) -> Tuple[Tensor, int]:
-        img = self.transform(Image.open(f"{self.root}/{self.data.image_path[item]}").convert("RGB"))
+        img = self.transform(Image.open(self.root / self.data.image_path[item]).convert("RGB"))
         label = self.data.label[item]
 
         return img, label

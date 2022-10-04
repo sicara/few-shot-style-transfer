4bc20af9776517f3c48a077fe9f9d2585a285386
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-10-04 12:00:41 +0200

    adds test for translation_to_en.py

--- src/tests/test_abo_data_formatting.py
+++ src/tests/test_abo_data_formatting.py
@@ -1,21 +1,35 @@
 from src.abo_data_formatting import ABOFormatting
 import pandas as pd
+import numpy as np
+import pytest
 
 
 class TestReadMetadata:
     @staticmethod
-    def test_read_metadata_makes_expected_changes_to_metadata_df(path_to_resources):
+    def test_read_metadata_makes_metadata_df_with_the_expected_columns(path_to_resources):
         abo_formatting = ABOFormatting(path_to_resources)
         abo_formatting.read_metadata()
         expected_metadata_df = pd.read_csv(
             path_to_resources / "expected_read_metadata_for_listings_0.csv", index_col=0
         )
-        assert expected_metadata_df.equals(abo_formatting.metadata_df)
-        assert (
-            abo_formatting.metadata_df.columns == expected_metadata_df.columns
-        ).all()
+        assert (abo_formatting.metadata_df.columns == expected_metadata_df.columns).all()
 
 
-# class TranslationToEn:
-#    @staticmethod
-#    def test_t
+class TestTranslationToEn:
+    cases_grid = [
+        {"text": "multi-colored", "src_language": "en", "expected_text": "multicolored"},
+        {"text": "Blau", "src_language": "de", "expected_text": "blue"},
+        {"text": "rouge", "src_language": "xy", "expected_text": "red"},
+        {"text": "하얀색", "src_language": "ko", "expected_text": "white"},
+        {"text": "a", "src_language": "en", "expected_text": np.nan},
+    ]
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "text,src_language,expected_text",
+        [tuple(case.values()) for case in cases_grid],
+    )
+    def test_translation_to_en_returns_expected_str(text, src_language, expected_text, path_to_resources):
+        abo_formatting = ABOFormatting(path_to_resources)
+        returned_text = abo_formatting.translation_to_en(text, src_language)
+        np.testing.assert_equal(returned_text, expected_text)

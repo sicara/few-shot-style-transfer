1ea50683842be9c245c09233a2b92ca40fee3bf5
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-10-05 12:30:07 +0200

    explodes imagenet classes

--- src/abo_classes_selection.py
+++ src/abo_classes_selection.py
@@ -1,15 +1,18 @@
 from typing import Union
 from pathlib import Path
 import pandas as pd
+import re
 
 from src.config import ROOT_FOLDER
 
 
 class ClassesSelection:
-    def __init__(self, path_to_abo_dataset_csv: Union[Path, str]):
+    def __init__(self, path_to_abo_dataset_csv: Union[Path, str], path_to_imagenet_csv: Union[Path, str]):
         self.path_to_abo_dataset_csv = Path(path_to_abo_dataset_csv)
+        self.path_to_imagenet_csv = Path(path_to_imagenet_csv)
         self.abo_classes = pd.DataFrame()
         self.imagenet_classes = pd.DataFrame()
+        self.matched_classes = []
 
     def get_consistent_abo_classes(self):
         data = (
@@ -18,19 +21,16 @@ class ClassesSelection:
             .count()
             .reset_index(name="count")
         )
-        self.abo_classes = pd.DataFrame(
-            list(data[data["count"] >= 17].apply(lambda row: row.product_type.lower().replace("_", " "), axis=1)),
-            columns=["abo_class"],
+        self.abo_classes = list(
+            data[data["count"] >= 17].apply(lambda row: row.product_type.lower().replace("_", " "), axis=1)
         )
 
     def get_imagenet_classes(self):
-        self.imagenet_classes = pd.DataFrame(
-            list(
-                pd.read_csv(Path(ROOT_FOLDER / "src" / "datasets" / "imagenet_classes.csv"), sep="|").apply(
-                    lambda row: row["Class Name"].lower(), axis=1
-                )
-            ),
-            columns=["imagenet_class"],
+        data = pd.read_csv(self.path_to_imagenet_csv, sep="|")
+        self.imagenet_classes = list(
+            data.assign(**{"Class Name": data["Class Name"].str.split(",")})
+            .explode("Class Name")
+            .apply(lambda row: row["Class Name"].lower(), axis=1)
         )
 
     def show_matches(self):
@@ -46,4 +46,7 @@ class ClassesSelection:
         self.write_selected_classes_to_json()
 
 
-ClassesSelection(Path(ROOT_FOLDER / "src" / "datasets" / "gathered_abo_data.csv")).get_imagenet_classes()
+ClassesSelection(
+    Path(ROOT_FOLDER / "src" / "datasets" / "gathered_abo_data.csv"),
+    Path(ROOT_FOLDER / "src" / "datasets" / "imagenet_classes.csv"),
+).selected_classes_to_json()

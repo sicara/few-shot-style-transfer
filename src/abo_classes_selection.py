0ffb9d85d70cfa0ce4f7dfd716831fdf71781f31
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-10-05 15:36:19 +0200

    takes PR review into account & black

--- src/abo_classes_selection.py
+++ src/abo_classes_selection.py
@@ -8,28 +8,36 @@ from src.config import ROOT_FOLDER
 
 
 class ClassesSelection:
-    def __init__(self, path_to_abo_dataset_csv: Union[Path, str], path_to_imagenet_csv: Union[Path, str]):
+    def __init__(
+        self,
+        path_to_abo_dataset_csv: Union[Path, str],
+        path_to_imagenet_csv: Union[Path, str],
+    ):
         self.path_to_abo_dataset_csv = Path(path_to_abo_dataset_csv)
         self.path_to_imagenet_csv = Path(path_to_imagenet_csv)
-        self.abo_classes = pd.DataFrame()
-        self.imagenet_classes = pd.DataFrame()
+        self.abo_classes = []
+        self.imagenet_classes = []
         self.matched_classes = []
 
     def get_consistent_abo_classes(self):
-        data = (
+        abo_class_df = (
             pd.read_csv(self.path_to_abo_dataset_csv)
             .groupby("product_type")["product_type"]
             .count()
             .reset_index(name="count")
         )
         self.abo_classes = list(
-            data[data["count"] >= 17].apply(lambda row: row.product_type.lower().replace("_", " "), axis=1)
+            abo_class_df[abo_class_df["count"] >= 17].apply(
+                lambda row: row.product_type.lower().replace("_", " "), axis=1
+            )
         )
 
     def get_imagenet_classes(self):
-        data = pd.read_csv(self.path_to_imagenet_csv, sep="|")
+        imagenet_class_df = pd.read_csv(self.path_to_imagenet_csv, sep="|")
         self.imagenet_classes = list(
-            data.assign(**{"Class Name": data["Class Name"].str.split(",")})
+            imagenet_class_df.assign(
+                **{"Class Name": imagenet_class_df["Class Name"].str.split(",")}
+            )
             .explode("Class Name")
             .apply(lambda row: row["Class Name"].lower(), axis=1)
         )
@@ -42,7 +50,9 @@ class ClassesSelection:
             return True
         elif user_answer.lower() != "n":
             self.ask_for_user_input(
-                abo_class, imagenet_class, information_sentence="Only 'y' and 'n' answers are allowed."
+                abo_class,
+                imagenet_class,
+                information_sentence="Only 'y' and 'n' answers are allowed.",
             )
         else:
             return False
@@ -53,15 +63,26 @@ class ClassesSelection:
                 if abo_class == imagenet_class:
                     self.matched_classes.append(abo_class)
                     break
-                elif re.search(r"(?:^|\W)" + (str(abo_class)) + r"(?:$|\W)", str(imagenet_class)):
+                elif re.search(
+                    r"(?:^|\W)" + (str(abo_class)) + r"(?:$|\W)", str(imagenet_class)
+                ):
                     stop_bool = self.ask_for_user_input(abo_class, imagenet_class)
                     if stop_bool:
                         break
 
     def write_selected_classes_to_json(self):
         selected_classes = list(set(self.abo_classes) - set(self.matched_classes))
-        with open(ROOT_FOLDER / "src/selected_and_matched_abo_classes_new.json", "w") as f:
-            json.dump({"matched": self.matched_classes, "selected": selected_classes, "manually_eliminated": []}, f)
+        with open(
+            ROOT_FOLDER / "src/selected_and_matched_abo_classes_new.json", "w"
+        ) as f:
+            json.dump(
+                {
+                    "matched": self.matched_classes,
+                    "selected": selected_classes,
+                    "manually_eliminated": [],
+                },
+                f,
+            )
 
     def selected_classes_to_json(self):
         self.get_consistent_abo_classes()
--- src/tests/test_abo_data_formatting.py
+++ src/tests/test_abo_data_formatting.py
@@ -6,18 +6,26 @@ import pytest
 
 class TestReadMetadata:
     @staticmethod
-    def test_read_metadata_makes_metadata_df_with_the_expected_columns(path_to_resources):
+    def test_read_metadata_makes_metadata_df_with_the_expected_columns(
+        path_to_resources,
+    ):
         abo_formatting = ABOFormatting(path_to_resources)
         abo_formatting.read_metadata()
         expected_metadata_df = pd.read_csv(
             path_to_resources / "expected_read_metadata_for_listings_0.csv", index_col=0
         )
-        assert (abo_formatting.metadata_df.columns == expected_metadata_df.columns).all()
+        assert (
+            abo_formatting.metadata_df.columns == expected_metadata_df.columns
+        ).all()
 
 
 class TestTranslationToEn:
     cases_grid = [
-        {"text": "multi-colored", "src_language": "en", "expected_text": "multicolored"},
+        {
+            "text": "multi-colored",
+            "src_language": "en",
+            "expected_text": "multicolored",
+        },
         {"text": "Blau", "src_language": "de", "expected_text": "blue"},
         {"text": "rouge", "src_language": "xy", "expected_text": "red"},
         {"text": "하얀색", "src_language": "ko", "expected_text": "white"},
@@ -29,7 +37,9 @@ class TestTranslationToEn:
         "text,src_language,expected_text",
         [tuple(case.values()) for case in cases_grid],
     )
-    def test_translation_to_en_returns_expected_str(text, src_language, expected_text, path_to_resources):
+    def test_translation_to_en_returns_expected_str(
+        text, src_language, expected_text, path_to_resources
+    ):
         abo_formatting = ABOFormatting(path_to_resources)
         returned_text = abo_formatting.translation_to_en(text, src_language)
         np.testing.assert_equal(returned_text, expected_text)

0ba8853ad0eac9e7724cafcb262005db81b32552
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-10-06 11:28:21 +0200

    adds the json and notebook to select colors

--- notebooks/find_colors_to_keep.py
+++ notebooks/find_colors_to_keep.py
@@ -11,7 +11,7 @@ color_count_df = pd.DataFrame(
     {"en_color": abo_data_df["en_color"].value_counts().index, "count": abo_data_df["en_color"].value_counts().values}
 )
 print("Number of different colors:", len(color_count_df))
-color_count_df[color_count_df["count"] > 100].head(15)
+color_count_df[color_count_df["count"] > 5]
 # %%
 color_count_df[color_count_df["count"] > 100].plot.bar(
     x="en_color", y="count", rot=90, figsize=(16, 2), title="Number of samples for each color (min 100)"
@@ -20,4 +20,7 @@ plt.show()
 # %%
 with open(ROOT_FOLDER / "src/selected_and_removed_colors.json") as json_file:
     selected_colors = json.load(json_file)["selected"]
+# %%
+print(list(color_count_df.en_color))
+
 # %%

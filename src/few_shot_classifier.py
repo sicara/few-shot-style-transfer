01f03787642a093e24a254ffa4c0fa6157428d34
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-10-12 17:23:23 +0200

    use statistics library to compute std and accuracy

--- src/few_shot_classifier.py
+++ src/few_shot_classifier.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 from src.style_transfer.fast_photo_style import FastPhotoStyle
 from src.config import ROOT_FOLDER
+from statistics import mean, stdev
 
 
 class EvaluatorFewShotClassifier:
@@ -21,9 +22,7 @@ class EvaluatorFewShotClassifier:
         """
         Returns the number of correct predictions of query labels, and the total number of predictions.
         """
-        self.few_shot_model.process_support_set(
-            support_images.cuda(), support_labels.cuda()
-        )
+        self.few_shot_model.process_support_set(support_images.cuda(), support_labels.cuda())
         return (
             torch.max(
                 self.few_shot_model(query_images.cuda()).detach().data,
@@ -32,23 +31,8 @@ class EvaluatorFewShotClassifier:
             == query_labels.cuda()
         ).sum().item(), len(query_labels)
 
-    def compute_standard_deviation(
-        self, total_predictions, correct_predictions, accuracy
-    ):
-        std = 0
-        number_of_tasks = len(total_predictions)
-        for i in range(number_of_tasks):
-            std += (
-                (100 * correct_predictions[i] / total_predictions[i]) - accuracy
-            ) ** 2
-        return np.sqrt(std / number_of_tasks)
-
-    def evaluate(
-        self, data_loader: DataLoader, style_transfer_augmentation: bool = False
-    ):
-        # We'll count everything and compute the ratio at the end
-        total_predictions = []
-        correct_predictions = []
+    def evaluate(self, data_loader: DataLoader, style_transfer_augmentation: bool = False):
+        accuracy = []
 
         # eval mode affects the behaviour of some layers (such as batch normalization or dropout)
         # no_grad() tells torch not to keep in memory the whole computational graph (it's more lightweight this way)
@@ -65,20 +49,10 @@ class EvaluatorFewShotClassifier:
                     (
                         support_images,
                         support_labels,
-                    ) = FastPhotoStyle().augment_support_set(
-                        support_images, support_labels
-                    )
-                correct, total = self.evaluate_on_one_task(
-                    support_images, support_labels, query_images, query_labels
-                )
-
-                total_predictions.append(total)
-                correct_predictions.append(correct)
-        accuracy = 100 * sum(correct_predictions) / sum(total_predictions)
-        std = self.compute_standard_deviation(
-            total_predictions, correct_predictions, accuracy
-        )
+                    ) = FastPhotoStyle().augment_support_set(support_images, support_labels)
+                correct, total = self.evaluate_on_one_task(support_images, support_labels, query_images, query_labels)
+                accuracy.append(100 * correct / total)
 
         print(
-            f"Model tested on {len(data_loader)} tasks. Accuracy: {accuracy:.2f}% +/- {(1.96*std/np.sqrt(len(total_predictions))):.2f}"
+            f"Model tested on {len(data_loader)} tasks. Accuracy: {mean(accuracy):.2f}% +/- {(1.96*stdev(accuracy)/np.sqrt(len(accuracy))):.2f}"
         )

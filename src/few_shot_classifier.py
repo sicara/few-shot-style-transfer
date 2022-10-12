b5c1d082d858af6e5b15ebc08ed38dc8d973646a
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-10-12 12:00:39 +0200

    add confiance intervall computation in EvaluatorFewShotClassifier

--- src/few_shot_classifier.py
+++ src/few_shot_classifier.py
@@ -21,24 +21,26 @@ class EvaluatorFewShotClassifier:
         """
         Returns the number of correct predictions of query labels, and the total number of predictions.
         """
+        self.few_shot_model.process_support_set(support_images.cuda(), support_labels.cuda())
         return (
             torch.max(
-                self.few_shot_model(
-                    support_images.cuda(), support_labels.cuda(), query_images.cuda()
-                )
-                .detach()
-                .data,
+                self.few_shot_model(query_images.cuda()).detach().data,
                 1,
             )[1]
             == query_labels.cuda()
         ).sum().item(), len(query_labels)
 
-    def evaluate(
-        self, data_loader: DataLoader, style_transfer_augmentation: bool = False
-    ):
+    def compute_standard_deviation(self, total_predictions, correct_predictions, accuracy):
+        std = 0
+        number_of_tasks = len(total_predictions)
+        for i in range(number_of_tasks):
+            std += ((100 * correct_predictions[i] / total_predictions[i]) - accuracy) ** 2
+        return np.sqrt(std / number_of_tasks)
+
+    def evaluate(self, data_loader: DataLoader, style_transfer_augmentation: bool = False):
         # We'll count everything and compute the ratio at the end
-        total_predictions = 0
-        correct_predictions = 0
+        total_predictions = []
+        correct_predictions = []
 
         # eval mode affects the behaviour of some layers (such as batch normalization or dropout)
         # no_grad() tells torch not to keep in memory the whole computational graph (it's more lightweight this way)
@@ -52,16 +54,16 @@ class EvaluatorFewShotClassifier:
                 class_ids,
             ) in tqdm(enumerate(data_loader), total=len(data_loader)):
                 if style_transfer_augmentation:
-                    support_images, support_labels = FastPhotoStyle(
-                        ROOT_FOLDER / "src/style_transfer"
-                    ).augment_support_set(support_images, support_labels)
-                correct, total = self.evaluate_on_one_task(
-                    support_images, support_labels, query_images, query_labels
-                )
+                    support_images, support_labels = FastPhotoStyle().augment_support_set(
+                        support_images, support_labels
+                    )
+                correct, total = self.evaluate_on_one_task(support_images, support_labels, query_images, query_labels)
 
-                total_predictions += total
-                correct_predictions += correct
+                total_predictions.append(total)
+                correct_predictions.append(correct)
+        accuracy = 100 * sum(correct_predictions) / sum(total_predictions)
+        std = self.compute_standard_deviation(total_predictions, correct_predictions, accuracy)
 
         print(
-            f"Model tested on {len(data_loader)} tasks. Accuracy: {(100 * correct_predictions/total_predictions):.2f}%"
+            f"Model tested on {len(data_loader)} tasks. Accuracy: {accuracy:.2f}% +/- {(1.96*std/np.sqrt(len(total_predictions))):.2f}"
         )

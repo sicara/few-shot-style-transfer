b908be68d56b07f4dd435a40f3aa66dee6c2ee3d
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-11-14 17:55:29 +0100

    add a color aware task sampler

---
+++ notebooks/task_sampler_color_aware.py
@@ -0,0 +1,58 @@
+#%%
+from src.abo import ABO
+from src.style_transfer.fast_photo_style import FastPhotoStyle
+from src.few_shot_classifier import EvaluatorFewShotClassifier
+from pathlib import Path
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from src.color_aware_task_sampling import ColorAwareTaskSampler
+from easyfsl.utils import plot_images
+from torch import nn
+from torchvision.models import resnet18
+from easyfsl.methods.prototypical_networks import PrototypicalNetworks
+
+#%%
+root = Path("data/abo_dataset/images/small")
+image_size = 112
+
+dataset = ABO(
+    root=root,
+    transform=transforms.Compose(
+        [
+            transforms.RandomResizedCrop(image_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ]
+    ),
+    classes_json=Path("data/selected_and_matched_abo_classes.json"),
+    colors_json=Path("data/selected_and_removed_colors.json"),
+)
+N_WAY = 2  # Number of classes in a task
+N_SHOT = 1  # Number of images per class in the support set
+N_QUERY = 16  # Number of images per class in the query set
+N_EVALUATION_TASKS = 100
+#%%
+test_sampler = ColorAwareTaskSampler(
+    dataset, n_shot=N_SHOT, n_query=N_QUERY, n_tasks=N_EVALUATION_TASKS
+)
+test_loader = DataLoader(
+    dataset,
+    batch_sampler=test_sampler,
+    num_workers=12,
+    pin_memory=True,
+    collate_fn=test_sampler.episodic_collate_fn,
+)
+(
+    example_support_images,
+    example_support_labels,
+    example_support_colors,
+    example_query_images,
+    example_query_labels,
+    example_query_colors,
+    example_true_class_ids,
+) = next(iter(test_loader))
+#%%
+plot_images(example_support_images, "support images", images_per_row=N_SHOT)
+plot_images(example_query_images, "query images", images_per_row=N_QUERY)
+
+# %%
--- src/color_aware_task_sampling.py
+++ src/color_aware_task_sampling.py
@@ -1,6 +1,8 @@
-from typing import List, Iterator
+from typing import List, Iterator, Tuple
 import torch
+from torch import Tensor
 import random
+import numpy as np
 from loguru import logger
 import pandas as pd
 
@@ -18,11 +20,9 @@ class ColorAwareTaskSampler(TaskSampler):
     def __init__(
         self,
         dataset: ABO,
-        n_way: int,
         n_shot: int,
         n_query: int,
         n_tasks: int,
-        n_colors: int = 2,
     ):
         """
         Args:
@@ -35,12 +35,11 @@ class ColorAwareTaskSampler(TaskSampler):
             n_colors (int, optional): Number of different colors used for one task (e.g. if equals 2, then one
                 class is represented by color A and all other classes are represented by color B). Defaults to 2.
         """
-        super().__init__(dataset, n_way, n_shot, n_query, n_tasks)
-        if n_colors > n_way:
-            logger.info(
-                f"Only {n_way} different color(s) used, because we only have {n_way} class(es)"
-            )
-        self.n_colors = n_colors
+        self.n_way = 2
+        self.n_shot = n_shot
+        self.n_query = n_query
+        self.n_tasks = n_tasks
+        self.n_colors = 2
 
         self.items_df = pd.DataFrame(
             {
@@ -52,27 +51,164 @@ class ColorAwareTaskSampler(TaskSampler):
 
     def __iter__(self) -> Iterator[List[int]]:
         for _ in range(self.n_tasks):
-            color_list = random.sample(
-                list(set(self.dataset.get_colors())), self.n_colors
+            # HOW.1
+            color_A = random.sample(list(self.items_df["color"].unique()), 1)[0]
+            # HOW.2
+            possible_classes = (
+                self.items_df.loc[self.items_df["color"] == color_A]
+                .groupby(["label"])["color"]
+                .count()
+                .reset_index(name="count")
             )
-            yield torch.cat(
-                [
-                    # pylint: disable=not-callable
-                    torch.tensor(
-                        random.sample(
-                            list(
-                                self.items_df.loc[
-                                    (self.items_df["label"] == label)
-                                    & (self.items_df["color"] in color_list),
-                                    ["item"],
-                                ]
-                            ),
-                            self.n_shot + self.n_query,
-                        )
-                    )
-                    # pylint: enable=not-callable
-                    for label in random.sample(
-                        list(self.items_df["label"].unique()), self.n_way
-                    )
+            possible_classes = list(
+                possible_classes.loc[possible_classes["count"] >= 8]["label"]
+            )
+            class_1 = random.sample(possible_classes, 1)[0]
+            # HOW.3
+            possible_classes_with_enough_colors = (
+                self.items_df.loc[
+                    (self.items_df["label"].isin(possible_classes))
+                    & (self.items_df["color"] != color_A)
                 ]
-            ).tolist()
+                .groupby(["color", "label"])["label"]
+                .count()
+                .reset_index(name="count")
+            )
+            class_2 = random.sample(
+                list(
+                    possible_classes_with_enough_colors.loc[
+                        possible_classes_with_enough_colors["count"] >= 8
+                    ]["label"]
+                ),
+                1,
+            )[0]
+            color_B = random.sample(
+                list(
+                    possible_classes_with_enough_colors.loc[
+                        (possible_classes_with_enough_colors["label"] == class_2)
+                        & (possible_classes_with_enough_colors["count"] >= 8)
+                    ]["color"]
+                ),
+                1,
+            )[0]
+            # HOW.4
+            items_A_1 = list(
+                self.items_df.loc[
+                    (self.items_df["color"] == color_A)
+                    & (self.items_df["label"] == class_1)
+                ]["item"]
+            )
+            if len(items_A_1) >= 9:
+                items_A_1 = random.sample(items_A_1, 9)
+            else:
+                items_A_1 = items_A_1 + random.sample(
+                    list(
+                        self.items_df.loc[
+                            (self.items_df["label"] == class_1)
+                            & (self.items_df["color"] != color_A)
+                        ]["item"]
+                    ),
+                    9 - len(items_A_1),
+                )
+            items_A_2 = random.sample(
+                list(
+                    self.items_df.loc[
+                        (self.items_df["color"] == color_A)
+                        & (self.items_df["label"] == class_2)
+                    ]["item"]
+                ),
+                8,
+            )
+            items_B_2 = list(
+                self.items_df.loc[
+                    (self.items_df["color"] == color_B)
+                    & (self.items_df["label"] == class_2)
+                ]["item"]
+            )
+            if len(items_B_2) >= 9:
+                items_B_2 = random.sample(items_B_2, 9)
+            else:
+                items_B_2 = items_B_2 + random.sample(
+                    list(
+                        self.items_df.loc[
+                            (self.items_df["label"] == class_2)
+                            & (self.items_df["color"] != color_B)
+                        ]["item"]
+                    ),
+                    9 - len(items_B_2),
+                )
+            items_B_1 = list(
+                self.items_df.loc[
+                    (self.items_df["color"] == color_B)
+                    & (self.items_df["label"] == class_1)
+                ]["item"]
+            )
+            if len(items_B_1) >= 8:
+                items_B_1 = random.sample(items_B_1, 8)
+            else:
+                items_B_1 = items_B_1 + random.sample(
+                    list(
+                        self.items_df.loc[
+                            (self.items_df["label"] == class_1)
+                            & (self.items_df["color"] != color_B)
+                        ]["item"]
+                    ),
+                    8 - len(items_B_1),
+                )
+            yield items_A_1 + items_B_1 + items_A_2 + items_B_2
+
+    def episodic_collate_fn(
+        self, input_data: List[Tuple[Tensor, int, str]]
+    ) -> Tuple[Tensor, Tensor, List[str], Tensor, Tensor, List[str], List[int]]:
+        """
+        Collate function to be used as argument for the collate_fn parameter of episodic
+            data loaders.
+        Args:
+            input_data: each element is a tuple containing:
+                - an image as a torch Tensor
+                - the label of this image
+                - the color of this image
+        Returns:
+            tuple(Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, list[int]): respectively:
+                - support images,
+                - their labels,
+                - their colors,
+                - query images,
+                - their labels,
+                - their colors,
+                - the dataset class ids of the class sampled in the episode
+        """
+        true_class_ids = list({x[1] for x in input_data})
+        colors_list = list(x[2] for x in input_data)
+
+        all_images = torch.cat([x[0].unsqueeze(0) for x in input_data])
+        all_images = all_images.reshape(
+            (self.n_way, self.n_shot + self.n_query, *all_images.shape[1:])
+        )
+        # pylint: disable=not-callable
+        all_labels = torch.tensor(
+            [true_class_ids.index(x[1]) for x in input_data]
+        ).reshape((self.n_way, self.n_shot + self.n_query))
+        # pylint: enable=not-callable
+
+        support_images = all_images[:, : self.n_shot].reshape(
+            (-1, *all_images.shape[2:])
+        )
+        query_images = all_images[:, self.n_shot :].reshape((-1, *all_images.shape[2:]))
+        support_labels = all_labels[:, : self.n_shot].flatten()
+        query_labels = all_labels[:, self.n_shot :].flatten()
+        support_colors = [colors_list[0], colors_list[self.n_query + self.n_shot]]
+        query_colors = (
+            colors_list[1 : self.n_query + 1]
+            + colors_list[self.n_query + self.n_shot + 1 :]
+        )
+
+        return (
+            support_images,
+            support_labels,
+            support_colors,
+            query_images,
+            query_labels,
+            query_colors,
+            true_class_ids,
+        )

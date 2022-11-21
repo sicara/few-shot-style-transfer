4798c39935f4f74745986b393876a5f2fce62ce7
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-11-21 18:06:41 +0100

    updates test for color aware task sampling

--- src/tests/test_color_aware_task_sampler.py
+++ src/tests/test_color_aware_task_sampler.py
@@ -1,6 +1,7 @@
 from src.color_aware_task_sampling import ColorAwareTaskSampler
 from src.abo import ABO
 from pathlib import Path
+import random
 from torchvision import transforms
 import pandas as pd
 
@@ -17,58 +18,101 @@ ABO_DATASET = ABO(
     colors_json=Path("data/selected_and_removed_colors.json"),
 )
 
-COLOR_AWARE_TASK_SAMPLER = ColorAwareTaskSampler(ABO_DATASET, n_query=16, n_tasks=100)
-
 
 class TestIter:
     @staticmethod
     def test_total_number_of_samples():
-        assert len(next(iter(COLOR_AWARE_TASK_SAMPLER))) == 34
+        color_aware_task_sampler = ColorAwareTaskSampler(
+            ABO_DATASET, n_query=16, n_tasks=100
+        )
+        random.seed(1)
+        test_list = []
+        for _ in range(5):
+            test_list.append(len(next(iter(color_aware_task_sampler))))
+        assert test_list == [34 for _ in range(5)]
 
     @staticmethod
     def test_min_number_of_colors():
-        task_data = next(iter(COLOR_AWARE_TASK_SAMPLER))
-        colors_list = []
-        for sample in task_data:
-            color = ABO_DATASET.get_item_color(sample)
-            if color not in colors_list:
-                colors_list.append(color)
-        assert len(colors_list) >= 2
+        color_aware_task_sampler = ColorAwareTaskSampler(
+            ABO_DATASET, n_query=16, n_tasks=100
+        )
+        random.seed(1)
+        test_list = []
+        for _ in range(5):
+            task_data = next(iter(color_aware_task_sampler))
+            colors_list = []
+            for sample in task_data:
+                color = ABO_DATASET.get_item_color(sample)
+                if color not in colors_list:
+                    colors_list.append(color)
+            test_list.append(len(colors_list) >= 2)
+        assert test_list == [True for _ in range(5)]
 
     @staticmethod
     def test_number_of_labels():
-        task_data = next(iter(COLOR_AWARE_TASK_SAMPLER))
-        label_list = []
-        for sample in task_data:
-            label = ABO_DATASET.get_item_label(sample)
-            if label not in label_list:
-                label_list.append(label)
-        assert len(label_list) == 2
+        color_aware_task_sampler = ColorAwareTaskSampler(
+            ABO_DATASET, n_query=16, n_tasks=100
+        )
+        random.seed(1)
+        test_list = []
+        for _ in range(5):
+            task_data = next(iter(color_aware_task_sampler))
+            label_list = []
+            for sample in task_data:
+                label = ABO_DATASET.get_item_label(sample)
+                if label not in label_list:
+                    label_list.append(label)
+            test_list.append(len(label_list))
+        assert test_list == [2 for _ in range(5)]
 
     @staticmethod
-    def test_support_colors_in_all_classes():
-        task_data = next(iter(COLOR_AWARE_TASK_SAMPLER))
-        support_colors = [
-            ABO_DATASET.get_item_color(task_data[0]),
-            ABO_DATASET.get_item_color(task_data[17]),
-        ]
-        class_1_colors = []
-        for class_1_sample in task_data[1:17]:
-            color = ABO_DATASET.get_item_color(class_1_sample)
-            if color not in class_1_colors and color in support_colors:
-                class_1_colors.append(color)
-        class_2_colors = []
-        for class_2_sample in task_data[18:]:
-            color = ABO_DATASET.get_item_color(class_2_sample)
-            if color not in class_2_colors and color in support_colors:
-                class_2_colors.append(color)
-        assert len(class_1_colors) == 2 and len(class_2_colors) == 2
+    def test_color_A_in_class_1():
+        color_aware_task_sampler = ColorAwareTaskSampler(
+            ABO_DATASET, n_query=16, n_tasks=100
+        )
+        random.seed(1)
+        test_list = []
+        for _ in range(5):
+            task_data = next(iter(color_aware_task_sampler))
+            color_A = ABO_DATASET.get_item_color(task_data[0])
+            class_1_color_A = 0
+            for class_1_sample in task_data[1:17]:
+                color = ABO_DATASET.get_item_color(class_1_sample)
+                if color == color_A:
+                    class_1_color_A += 1
+            test_list.append(class_1_color_A >= 8)
+        assert test_list == [True for _ in range(5)]
+
+    @staticmethod
+    def test_color_B_in_class_2():
+        color_aware_task_sampler = ColorAwareTaskSampler(
+            ABO_DATASET, n_query=16, n_tasks=100
+        )
+        random.seed(1)
+        test_list = []
+        for _ in range(5):
+            task_data = next(iter(color_aware_task_sampler))
+            color_B = ABO_DATASET.get_item_color(task_data[17])
+            class_2_color_B = 0
+            for class_2_sample in task_data[17:]:
+                color = ABO_DATASET.get_item_color(class_2_sample)
+                if color == color_B:
+                    class_2_color_B += 1
+            test_list.append(class_2_color_B >= 8)
+        assert test_list == [True for _ in range(5)]
 
     @staticmethod
     def test_support_colors_are_different():
-        task_data = next(iter(COLOR_AWARE_TASK_SAMPLER))
-        support_colors = [
-            ABO_DATASET.get_item_color(task_data[0]),
-            ABO_DATASET.get_item_color(task_data[17])[2],
-        ]
-        assert support_colors[0] != support_colors[1]
+        color_aware_task_sampler = ColorAwareTaskSampler(
+            ABO_DATASET, n_query=16, n_tasks=100
+        )
+        random.seed(1)
+        test_list = []
+        for _ in range(5):
+            task_data = next(iter(color_aware_task_sampler))
+            support_colors = [
+                ABO_DATASET.get_item_color(task_data[0]),
+                ABO_DATASET.get_item_color(task_data[17])[2],
+            ]
+            test_list.append(support_colors[0] != support_colors[1])
+        assert test_list == [True for _ in range(5)]

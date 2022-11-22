869f2b41432923d11466393fc6cf464d3ff07ffb
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-11-22 15:19:01 +0100

    adds job dataset to assess true&predicted labels and colors

--- src/few_shot_classifier.py
+++ src/few_shot_classifier.py
@@ -1,11 +1,11 @@
-import torch
-from torch import nn
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
+import torch
 from torch.utils.data import DataLoader
-from src.style_transfer.fast_photo_style import FastPhotoStyle
-from src.config import ROOT_FOLDER
 from statistics import mean, stdev
+
+from src.style_transfer.fast_photo_style import FastPhotoStyle
 
 
 class EvaluatorFewShotClassifier:
@@ -76,7 +76,7 @@ class EvaluatorFewShotClassifierWColor:
         support_labels: torch.Tensor,
         query_images: torch.Tensor,
         query_labels: torch.Tensor,
-    ) -> [int, int]:
+    ) -> [torch.Tensor, int]:
         """
         Returns the number of correct predictions of query labels, and the total number of predictions.
         """
@@ -88,14 +88,22 @@ class EvaluatorFewShotClassifierWColor:
                 self.few_shot_model(query_images.cuda()).detach().data,
                 1,
             )[1]
-            == query_labels.cuda()
-        ).sum().item(), len(query_labels)
+        ), len(query_labels)
 
     def evaluate(
         self, data_loader: DataLoader, style_transfer_augmentation: bool = False
-    ):
+    ) -> pd.DataFrame:
         accuracy = []
-
+        job_dataset = pd.DataFrame(
+            {
+                "task_id": [],
+                "true_label": [],
+                "predicted_label": [],
+                "color": [],
+                "support_set_1_color": [],
+                "support_set_2_color": [],
+            }
+        )
         # eval mode affects the behaviour of some layers (such as batch normalization or dropout)
         # no_grad() tells torch not to keep in memory the whole computational graph (it's more lightweight this way)
         self.few_shot_model.eval()
@@ -116,11 +124,27 @@ class EvaluatorFewShotClassifierWColor:
                     ) = FastPhotoStyle().augment_support_set(
                         support_images, support_labels
                     )
-                correct, total = self.evaluate_on_one_task(
+                prediction, total = self.evaluate_on_one_task(
                     support_images, support_labels, query_images, query_labels
                 )
+                correct = prediction.sum().item()
                 accuracy.append(100 * correct / total)
-
+                task_dataset = pd.DataFrame(
+                    {
+                        "task_id": [episode_index for _ in range(len(prediction))],
+                        "true_label": query_labels,
+                        "predicted_label": prediction.tolist(),
+                        "color": query_colors,
+                        "support_set_1_color": [
+                            support_colors[0] for _ in range(len(prediction))
+                        ],
+                        "support_set_2_color": [
+                            support_colors[1] for _ in range(len(prediction))
+                        ],
+                    }
+                )
+                job_dataset = pd.concat([job_dataset, task_dataset], ignore_index=True)
         print(
             f"Model tested on {len(data_loader)} tasks. Accuracy: {mean(accuracy):.2f}% +/- {(1.96*stdev(accuracy)/np.sqrt(len(accuracy))):.2f}"
         )
+        return job_dataset

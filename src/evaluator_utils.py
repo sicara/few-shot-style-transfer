b1e97479c6252a782955289ad6e26a6a02cd704c
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-12-28 11:40:53 +0100

    adds color similarity index and per task plot

--- src/evaluator_utils.py
+++ src/evaluator_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def compute_total_accuracy(query_results: pd.DataFrame) -> float:
@@ -101,3 +102,73 @@ def compute_accuracy_for_samples_with_same_color_as_no_class_representative(
         / len(query_results_same_color_as_no_class_representative)
         * 100
     )
+
+
+def compute_task_color_similarity(query_results: pd.DataFrame, task_id: int) -> float:
+    task_query_results = query_results[(query_results["task_id"] == task_id)]
+    return (
+        0.5
+        + len(
+            pd.concat(
+                [
+                    task_query_results[
+                        (task_query_results["true_label"] == 1)
+                        & (
+                            task_query_results["color"]
+                            == task_query_results["support_set_1_color"]
+                        )
+                    ],
+                    task_query_results[
+                        (task_query_results["true_label"] == 0)
+                        & (
+                            task_query_results["color"]
+                            == task_query_results["support_set_0_color"]
+                        )
+                    ],
+                ]
+            )
+        )
+        / len(task_query_results)
+        - len(
+            pd.concat(
+                [
+                    task_query_results[
+                        (task_query_results["true_label"] == 1)
+                        & (
+                            task_query_results["color"]
+                            == task_query_results["support_set_0_color"]
+                        )
+                    ],
+                    task_query_results[
+                        (task_query_results["true_label"] == 0)
+                        & (
+                            task_query_results["color"]
+                            == task_query_results["support_set_1_color"]
+                        )
+                    ],
+                ]
+            )
+        )
+        / len(task_query_results)
+    ) * 100
+
+
+def plot_task_accuracy_and_color_similarity(query_results: pd.DataFrame):
+    number_of_tasks = len(list(query_results["task_id"].unique()))
+    task_accuracy_list = [
+        compute_accuracy_for_one_task(query_results, task_id)
+        for task_id in range(number_of_tasks)
+    ]
+    task_color_similarity_list = [
+        compute_task_color_similarity(query_results, task_id)
+        for task_id in range(number_of_tasks)
+    ]
+    fig, ax = plt.subplots()
+    ax.plot(task_accuracy_list, color="red", marker="o")
+    ax.set_xlabel("task_id", fontsize=10)
+    ax.set_ylabel("accuracy", color="red", fontsize=10)
+    ax.set_ylim((0, 100))
+    ax2 = ax.twinx()
+    ax2.plot(task_color_similarity_list, color="blue", marker="o")
+    ax2.set_ylabel("color similarity index", color="blue", fontsize=10)
+    plt.show()

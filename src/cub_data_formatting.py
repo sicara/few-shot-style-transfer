28bb33085e9103a5221b6df0d2644f5d0acf461e
Mahaut Gerard <mahaut.gerard@sicara.com>
2022-12-30 10:53:23 +0100

    refacto cub dataset formating

--- src/cub_data_formatting.py
+++ src/cub_data_formatting.py
@@ -8,6 +8,11 @@ from src.config import ROOT_FOLDER
 class CUBFormatting:
     def __init__(self, path_to_cub_dataset_folder: Union[Path, str]):
         self.path_to_cub_dataset_folder = Path(path_to_cub_dataset_folder)
+        self.image_df = pd.DataFrame()
+        self.classes_df = pd.DataFrame()
+        self.class_attributes_df = pd.DataFrame()
+        self.attributes_df = pd.DataFrame()
+        self.image_annotation_df = pd.DataFrame()
         self.gathered_data_df = pd.DataFrame(
             columns=[
                 "image_id",
@@ -19,69 +24,119 @@ class CUBFormatting:
             ]
         )
 
-    def build_metadata_csv_from_raw_data(self):
-        images_df = pd.read_csv(
+    def read_and_format_images_data(self):
+        self.images_df = pd.read_csv(
             self.path_to_cub_dataset_folder / Path("images.txt"),
             sep=" ",
             header=None,
             names=["image_id", "path"],
         )
-        images_df["class_name"] = images_df.apply(
+        self.images_df["class_name"] = self.images_df.apply(
             lambda row: str(row["path"]).split("/")[0], axis=1
         )
-        classes_df = pd.read_csv(
+
+    def read_and_format_classes_data(self):
+        self.classes_df = pd.read_csv(
             self.path_to_cub_dataset_folder / Path("classes.txt"),
             sep=" ",
             header=None,
             names=["class_id", "class_name"],
         )
-        class_atributes_df = pd.read_csv(
+
+    def read_and_format_classes_attributes_data(self):
+        self.class_atributes_df = pd.read_csv(
             self.path_to_cub_dataset_folder
             / Path("attributes/class_attribute_labels_continuous.txt"),
             sep=" ",
             header=None,
             names=[str(i) for i in range(1, 313)],
         )
-        class_atributes_df["class_id"] = class_atributes_df.index
-        class_atributes_df["class_id"] = class_atributes_df.apply(
+        self.class_atributes_df["class_id"] = self.class_atributes_df.index
+        self.class_atributes_df["class_id"] = self.class_atributes_df.apply(
             lambda row: int(row.class_id + 1), axis=1
         )
-        class_atributes_df["color_attribute_id"] = class_atributes_df[
+        self.class_atributes_df["color_attribute_id"] = self.class_atributes_df[
             [str(i) for i in range(249, 264)]
         ].idxmax(axis=1)
-        attribute_df = pd.read_csv(
+
+    def read_and_format_attributes_data(self):
+        self.attribute_df = pd.read_csv(
             self.path_to_cub_dataset_folder / Path("classes.txt"),
             sep=" ",
             header=None,
             names=["attribute_id", "attribute_name"],
         )
-        attribute_df = pd.read_csv(
+        self.attribute_df = pd.read_csv(
             self.path_to_cub_dataset_folder / Path("attributes/attributes.txt"),
             sep=" ",
             header=None,
             names=["attribute_id", "attribute_name"],
         )
-        attribute_df["color"] = attribute_df.apply(
+        self.attribute_df["color"] = self.attribute_df.apply(
             lambda row: str(row["attribute_name"]).split("::")[-1], axis=1
         )
-        attribute_df["attribute_id"] = attribute_df.apply(
+        self.attribute_df["attribute_id"] = self.attribute_df.apply(
             lambda row: str(row.attribute_id), axis=1
         )
 
+    def read_and_format_images_annotation_data(self):
+        self.image_annotation_df = pd.read_csv(
+            self.path_to_cub_dataset_folder
+            / Path("attributes/image_attribute_labels.txt"),
+            sep=" ",
+            header=None,
+            names=[
+                "image_id",
+                "annotated_attribute_id",
+                "is_present",
+                "certainty",
+                "time",
+            ],
+            on_bad_lines="warn",
+        )
+        self.image_annotation_df = (
+            self.image_annotation_df[
+                (self.image_annotation_df["annotated_attribute_id"] >= 249)
+                & (self.image_annotation_df["annotated_attribute_id"] <= 263)
+                & (self.image_annotation_df["certainty"] >= 3)
+                & (self.image_annotation_df["is_present"] == 1)
+            ]
+            .groupby("image_id")
+            .first()
+            .reset_index()
+        )
+
+    def build_metadata_csv_from_raw_data(self):
+        self.read_and_format_images_data()
+        self.read_and_format_classes_data()
+        self.read_and_format_classes_attributes_data()
+        self.read_and_format_attributes_data()
+        self.read_and_format_images_annotation_data()
         self.gathered_data_df = (
-            images_df.merge(classes_df, how="left", on="class_name")
+            self.images_df.merge(self.classes_df, how="left", on="class_name")
             .merge(
-                class_atributes_df[["color_attribute_id", "class_id"]],
+                self.image_annotation_df[["image_id", "annotated_attribute_id"]],
+                how="left",
+                on="image_id",
+            )
+            .merge(
+                self.class_atributes_df[["color_attribute_id", "class_id"]],
                 how="left",
                 on="class_id",
             )
             .merge(
-                attribute_df[["attribute_id", "color"]],
+                self.attribute_df[["attribute_id", "color"]],
                 how="left",
                 left_on="color_attribute_id",
                 right_on="attribute_id",
             )
             .drop("attribute_id", axis=1)
+        )
+        self.gathered_data_df.annotated_attribute_id.fillna(
+            self.gathered_data_df.color_attribute_id, inplace=True
+        )
+        self.gathered_data_df = self.gathered_data_df.astype(
+            {"annotated_attribute_id": "int"}
         )
         # TODO: add color label for each image (not only for the class label)
         ## from image_attribute_labels.txt
